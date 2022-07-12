0c602b4ef2c9c64ca2b9cac9ceeca6378107b7d1
Sajal4 <sajaldebnath923@gmail.com>
2022-07-12 17:29:52 +0530

    first commit

---
+++ src/Component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <div className='container__header'>
+      <p>The</p>
+      <h1> Galaxy </h1>
+    </div>
+    </div>
+  )
+}
+
+export default Header
