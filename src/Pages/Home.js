108fb4b82c37201259b4fabdbab2e55ba7ccf26c
Sajal4 <sajaldebnath923@gmail.com>
2022-07-12 17:45:12 +0530

    fix commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -21,11 +21,11 @@ const Home = () => {
         <h1>Kerala:Gods Own Country</h1>
     </div>
         <div className="home__sec">
-        <Link to="/fitness" > <img src="https://images.pexels.com/photos/3490348/pexels-photo-3490348.jpeg?auto=compress&cs=tinysrgb&w=600" alt="not foun" /></Link>
+        <Link to="/fitness" > <img src="https://image.shutterstock.com/image-photo/muscular-arab-man-training-dumbbells-600w-1913720560.jpg" alt="not foun" /></Link>
         </div>
         </div>
         <div className="home__thir">
-        <Link to='/technology'>   <img src="https://media.istockphoto.com/photos/shot-of-a-young-man-using-his-digital-tablet-and-computer-in-a-modern-picture-id1344688192?b=1&k=20&m=1344688192&s=170667a&w=0&h=vvCGfVelYwrPia3dh2yH9rBSur2O9mD_kw2R-lbBIDw=" alt="not found" /></Link>
+        <Link to='/technology'>   <img src="https://initialcommit.com/img/initialcommit/frontend-development-skills.jpg" alt="not found" /></Link>
         </div>
         <h1 style={{marginTop:"40px"}}>The Latest</h1>
         <hr style={{width:"200px",  thickness:"20px"}} />
