97fad9db3844ca9599b05acece54c8955e96de87
Sajal4 <sajaldebnath923@gmail.com>
2022-07-12 17:36:56 +0530

    commit

---
+++ details/Bollywood.js
@@ -0,0 +1,98 @@
+const get = (req,res)=>{
+    res.send([
+        {
+            "id": "1",
+            "title": "Akshay Kumar's Prithviraj title changed to Samrat Prithviraj a week before its release",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/prithviraj41653729087.jpg",
+            "category": "Bollywood",
+            "description": "The title of Prithviraj, Akshay Kumar's upcoming film has been changed to Samrat Prithviraj. The film directed by Dr Chandraprakash Dwivedi is a sprawling period drama based on the life and times of Samrat Prithviraj. It features Kumar alongside former Miss World Manushi Chhillar, and contribution to our nation’s history, through this Film,It further addressed the president of Shree Rajput Karni Sena saying, “As per the multiple rounds of discussions between us, and to peacefully and amicably resolve the raised grievance, we will change the title of the Film to “Samrat Prithviraj. We are highly appreciative of the mutual agreement reached between us that you have no further objections with regards to our Film and that all other points raised by you earlier are no longer a point of contention between us. We thank Shri Rajput Karni Sena and its members for understanding our good intentions pertaining to the depiction of the great warrior in the Film."
+          },
+          {
+            "id": "51",
+            "title": "Akshay Kumar's Prithviraj title changed to Samrat Prithviraj a week before its release",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/prithviraj41653729087.jpg",
+            "category": "bollywood",
+            "description": "The title of Prithviraj, Akshay Kumar's upcoming film has been changed to Samrat Prithviraj. The film directed by Dr Chandraprakash Dwivedi is a sprawling period drama based on the life and times of Samrat Prithviraj. It features Kumar alongside former Miss World Manushi Chhillar, Sanjay Dutt, Sonu Sood and others. News of the film's title change comes a week ahead of its release.For the uninitiated, the Akshay Kumar-led  title of the Film to “Samrat Prithviraj. We are highly appreciative of the mutual agreement reached between us that you have no further objections with regards to our Film and that all other points raised by you earlier are no longer a point of contention between us. We thank Shri Rajput Karni Sena and its members for understanding our good intentions pertaining to the depiction of the great warrior in the Film."
+          },
+          {
+            "id": "2",
+            "title": "Deepika Padukone EXUDES desi vibes with a modern touch in a white ruffle saree during the closing ceremony",
+            "Image": "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Deepika-76.png",
+            "category": "Bollywood",
+            "description": "Gehraiyaan actress Deepika Padukone is among the few stars who are representing India at Cannes 2022. She is churning out some fabulous fashion stints her stint at Cannes 2022.The actress looked absolutely divine as walked the red carpet wearing this beautiful saree. The neckpiece made of pearls added much glam to the sweet saree.  The actress attended the press conference with other jury members Rebecca Hall and Jeff Nichols. Sticking to her roots, she greeted everyone with a namaste.Well, if this picture does not hypnotise you then we don't know what will. Subtle makeup, pearl earrings and more - the actress looked drop-dead gorgeous as ever."
+          },
+          {
+            "id": "61",
+            "title": "Deepika Padukone EXUDES desi vibes with a modern touch in a white ruffle saree during the closing ceremony",
+            "Image": "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Deepika-76.png",
+            "category": "footer1",
+            "description": "Gehraiyaan actress Deepika Padukone is among the few stars who are representing India at Cannes 2022. She is churning out some fabulous fashion stints leaving all her fans mesmerised. For the closing ceremony of the international film festival, the actress opted to go all desi.  of pearls added much glam to the sweet saree.  The actress attended the press conference with other jury members Rebecca Hall and Jeff Nichols. Sticking to her roots, she greeted everyone with a namaste.Well, if this picture does not hypnotise you then we don't know what will. Subtle makeup, pearl earrings and more - the actress looked drop-dead gorgeous as ever."
+          },
+          
+          {
+            "id": "3",
+            "title": "SS Rajamouli's RRR hailed by Eternals star Patton Oswalt: F**ken RRR is insane",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/shahrukhkhan41653712635.jpg",
+            "category": "Bollywood",
+            "description": "Hollywood comedian and actor Patton Oswalt cannot get enough of RRR. The actor who is best known for his Marvel roles including Eternals was left impressed with director SS Rajamouli's latest magnum opus. He recently took to Twitter to recommend the movie.RR was a megahit that had and NTR Jr and follows two revolutionaries - Alluri Sitarama Raju and Komaram Bheem as they go up against the British rule in India. The film was released on March 24 this year. Since then, it has garnered significant buzz. It also kickstarted a debate on the way pan-India films are perceived."
+          },
+          {
+            "id": "4",
+            "title": "Shah Rukh Khan, Salman Khan and Madhuri Dixit's selfie has fans recalling Hum Tumhare Hai Sanam",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/rrrpattonoswalt41653717404.jpg",
+            "category": "Bollywood",
+            "description": "Karan Johar's 50th birthday bash was one big star-studded celebration. The evening saw a bevvy of Bollywood celebrities making an appearance and with so many biggies coming together, reunions were inevitable. Now, a selfie of Shah Rukh Khan, Salman Khan and Madhuri Dixit has  The 2002 film was a romantic drama that revolved around a love triangle. Moreover, the pic also sparked nostalgia for Hum Aapke Hai Koun which saw Madhuri and Salman share screen space in an iconic pairing.Karan Johar’s birthday bash had several memorable moments. While Shah Rukh Khan did not walk the red carpet, videos of him dancing like nobody’s watching on Koi Mil Gaya from Kuch Kuch Hota Hai surfaced later. Meanwhile, Salman Khan who was one of the last guests to arrive reportedly stayed at the party till the morning."
+          },
+          {
+            "id": "5",
+            "title": "Akshay Kumar was all praise for Manushi Chillar’s sharp memory during the promotions of Prithviraj",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/akshaykumarwasallpraises41653652745.jpg",
+            "category": "Bollywood",
+            "description": "Akshay Kumar and Manushi Chhillar have kick-started promotions for their upcoming movie Prithviraj, which will be released on June 3. In a recent appearance on a comshay Kumar’s comedy movies, she strongly feels that Prithviraj is his best movie to date. She said, I am a big fan of sir’s comedy and the few comedy films that sir has done. But, after watching Prithviraj, I can definitely say that Prithviraj is my favourite film of sir."
+          },
+          {
+            "id": "6",
+            "title": "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
+            "category": "Bollywood",
+            "description": "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. While they are yet to make an announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others."
+          },
+          {
+            "id": "54",
+            "title": "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
+            "category": "mix",
+            "description": "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. Hrithik Roshan and Saba Azad have been spending a lot of time together and needless to say, they’ve been the talk of the town. While they are yet to make an announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others."
+          },
+          {
+            "id": "7",
+            "title": "Inside pictures from Karan Johar’s 50th birthday: Anushka Sharma and Ranbir Kapoor reunite",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/vickykaushal41653570327.jpg",
+            "category": "Bollywood",
+            "description": "Karan Johar’s 50th birthday bash is the talk of B-town. The event was attended by A-listers such as Salman Khan, Aamir Khan, Kareena Kapoor Khan, Saif Ali Khan, Kajol, Rani Mukerji, Shahid Kapoor, Mira Kapoor, Anushka Sharma, Gauri Khan, Hrithik Roshan, Ranveer Singh, Ranbir Kapoor among others.After the red-carpet pictures of celebrities riled up a storm, we are now looking at the inside pictures from the event. These pictures candidly show our favourite stars having the time of their lives. As Katrina Kaif and Vicky Kaushal dish out couple goals, Ranbir Kapoor and Anushka Sharma reunite. "
+          },
+        {
+          "id": "8",
+          "title": "Bollywood Alia Bhatt finally reacts to viral video of Ranbir Kapoor cradling a baby; cries 'happy tears'",
+          "Image": "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Alia-Ranbir-baby-video.jpg?impolicy=Medium_Widthonly&w=800",
+          "category": "Bollywood",
+          "description": "A video of Ranbir Kapoor cradling a baby had gone viral on the internet. In the video, Ranbir was seen carrying the little one in his arms, kisses on the head and continues. And the viral video of Ranbir's has finally got Alia's much-needed attention, and it got her happy tears."
+        },
+        {
+          "id": "9",
+          "title": "Bollywood Deepika Padukone looks jaw droppingly stunning in latest photos",
+          "Image": "https://www.pinkvilla.com/imageresize/deepika_69.jpg?width=752&format=webp&t=pvorg",
+          "category": "Bollywood",
+          "description": "Deepika Padukone never fails to impress us with her style. The stunning actress is an epitome of grace and charm. Deepika is the highest paid actress in the country today and has made it to the Top 5 of Forbes List of Richest Indian Celebs. The actor was featured in TIME magazine’s 100 Most Influential People List and is one of the few Indian actors to have attended coveted events abroad. "
+        },
+        {
+          "id": "10",
+          "title": "Bollywood Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya return ",
+          "Image": "https://www.pinkvilla.com/imageresize/abhishek%20bachchan%20aishwarya%20aaradhya%20%282%29.JPG?width=752&format=webp&t=pvorg",
+          "category": "Bollywood",
+          "description": "Aishwarya Rai Bachchan, Abhishek Bachchan and daughter Aaradhya returned to Mumbai today after ringing in 2019 abroad. Abhishek and Aishwarya shared photos from their New Year getaway as the family basked in the sun. At the airport, daughter Aaradhya was engaged in a conversation with her parents and looked cute as a button in a pink dress."
+        }
+    ])
+}
+
+module.exports.apiController = get;
---
+++ details/Home.js
@@ -0,0 +1,84 @@
+const get = (req,res)=>{
+    res.send([
+          {
+            "id": '52',
+          "title": "Why I fell in love with Microsoft’s Surface Laptop Studio despite its shortcomings ",
+          "Image": "https://images.indianexpress.com/2022/05/K1.jpg",
+          "category": "technology",
+          "description": "As I was waiting for someone at a Delhi hotel recently, I saw a gentleman coming my way. He sat next to me and curiously asked about the laptop I was using. also a sketching slate and a portable home theater. I have been using the Surface Laptop Studio for a few days, and I am in love with this device even though it is not perfect. Here is my review."
+          },
+          {
+            "id": '51',
+            "title": "Akshay Kumar's Prithviraj title changed to Samrat Prithviraj a week before its release",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/prithviraj41653729087.jpg",
+            "category": "bollywood",
+            "description": "The title of Prithviraj, Akshay Kumar's upcoming film has been changed to Samrat Prithviraj. The film directed by Dr Chandraprakash Dwivedi is a sprawling period drama based on the life and times of Samrat Prithviraj. It features Kumar alongside former Miss World Manushi Chhillar, Sanjay Dutt, Sonu Sood and others. News of the film's title change comes a week ahead of its release.For the uninitiated, and to peacefully and amicably resolve the raised grievance, we will change the title of the Film to “Samrat Prithviraj. We are highly appreciative of the mutual agreement reached between us that you have no further objections with regards to our Film and that all other points raised by you earlier are no longer a point of contention between us. We thank Shri Rajput Karni Sena and its members for understanding our good intentions pertaining to the depiction of the great warrior in the Film."
+          
+          },
+          {
+            "id": '46',
+            "title": "When to Have Breakfast, Lunch and Dinner to Get Maximum Benefits?",
+            "Image": "https://images.news18.com/ibnlive/uploads/2022/01/untitled-design-1-8-16412180333x2.png?impolicy=website&width=510&height=356",
+            "category": "food",
+            "description": "When we wake up, the first thing that comes to put mind is what to have for breakfast. After finishing it, we start thinking about lunch and then about dinner. This shows that food is one of the most important parts of our lives. One day we skip a meal, and our body starts reacting in a different way.  is the first meal of the day which makes our guts wake up. Usually, breakfast is eaten after 8 to 10 hours of having our dinner. The best time to have the first meal of the day is between 7 am to 9 am.Lunch: When you eat your breakfast early, it gives your stomach enough time to rest before you go on to your lunch. This aids digestion of breakfast. The best time to have lunch is between 12 noon to 2 pm. Dinner: As you have had your lunch early, you might feel hungry soon in the evening. Many dieticians recommend having early dinner in order to satisfy our hunger as well as boost metabolism. You should have your dinner between 6.30 pm to 8 pm.  If you have all your meals on a scheduled time daily, then it can help you boost your metabolism. In the morning, your metabolic rate works best, and as the day passes, it slows down. Therefore, having an early breakfast gives it a boost and an early dinner help it sustain."
+          },
+          {
+            "id": '58',
+            "title": "These Harmless Summer Drinks are Perfect for Diabetic Patients",
+            "Image": "https://images.news18.com/ibnlive/uploads/2022/05/summer-drink-16518193713x2.jpg?impolicy=website&width=510&height=356",
+            "category": "mix",
+            "description": "The summer season is here with soaring temperatures and dehydrating weather. And they call for cold and refreshing drinks. While many of these drinks are healthy and rehydrating, the sugar content in them might not be healthy for diabetic patients. This makes it difficult for people suffering from the chronic disease to stay hydrated and have all essential electrolytes up to their necessary levels. Here are 5 summer coolers that are healthy and contain zero sugar for diabetic patients to enjoy and help stay hydra have this tasty sugarless drink ready. Salted lassi is a summer cooler that diabetic patients can enjoy without the worry of their disease aggravating.Bel or wood apple is a great source of natural fibre, iron, antioxidants and folates. Along with this it cools your stomach. If you suffer from diabetes, bel sherbet can prove to be very beneficial for you during the scorching summers."
+          },
+          {
+            "id": '54',
+            "title": "It's official! Hrithik Roshan introduced Saba Azad as his girlfriend at Karan Johar's bash",
+            "Image": "https://images.hindustantimes.com/img/2022/05/28/550x309/dcf35a88-dd9e-11ec-8fe2-ac5ac3115257_1653667000472_1653747840857.jpg",
+            "category": "mix",
+            "description": "Talks of Hrithik Roshan and Saba Azad’s rumoured relationship have been doing the rounds for a while. But now, the two have made it red carpet official by appearing together at Karan Johar's grand birthday bash. Hrithik and Saba arrived at the party on theme in their black outfits and made for a stunning pair. Turns out, after gracing the red carpet, Hrithik also introduced Saba Azad as his girlfriendan announcement to confirm their relationship, it cannot get more obvious than this.Recently, Hrithik and Saba also attended a birthday dinner together with the Roshan family. Hrithik’s father Rakesh Roshan and mother Pinkie were present at the intimate family gathering. Meanwhile, Karan Johar’s bash which took place in Mumbai on May 25 was attended by the most popular Bollywood stars including Aishwarya Rai Bachchan and Abhishek Bachchan, Katrina Kaif and Vicky Kaushal, Anushka Sharma, Ranveer Singh, Ranbir Kapoor and others."
+          },
+          {
+            "id": '55',
+            "title": "Top Gun Maverick BO: Tom Cruise film collects $51 mn in US, ₹4.75 cr in India ",
+            "Image": "https://filmfare.wwmindia.com/content/2022/may/hrithikroshansabaazad41653633774.jpg",
+            "category": "mix",
+            "description": "Top Gun Maverick box office day 1 collection: Tom Cruise's film collected $51 million at the US box office on its opening day. In India, it collected ₹4.75 crore. Top Gun came out in 1986, establishing Cruise, then 24, as an action hero. He’s nearly 60 now, promoted to US Navy captain in the sequel, and working hard to recreate the magic from 36 years ago.Director Joseph Kosinski's Top Gun Maverick, which features Tom Cruise, had a contrasting opening at the box office in India and the US. In the US, the film collected $51 million on its opening day. The film is already Tom’s best opening day ever beating Mission Impossible – Fallout's first-day collection. However, the film has not been able to make a mark in India and collected ₹4.75 crore on day one. In Top Gun Maverick, Tom essays the role of Pete 'Maverick' Mitchell, who is grappling with the diminished standing of fighter pilots in a world that now wages war with drones controlled by people miles away on computers. Top Gun Maverick is produced by Jerry Bruckheimer, Tom Cruise, Christopher McQuarrie and David Ellison.As per a Deadline report, Top Gun Maverick at this Friday night hour is on its way to a $51 million opening day (which includes $19.3 million previews). It would not be shocking to wake up on Saturday morning and see that the sequel has edged out Iron Man 2 ($51.2M) to become Paramount’s top opening day ever. Already, the sequel is Tom Cruise’s best opening day ever in US/Canada besting Mission: Impossible – Fallout‘s first day of $22.8M by 124%."
+          },
+          {
+            "id": '56',
+            "title": "Why I fell in love with Microsoft’s Surface Laptop Studio despite its shortcomings ",
+            "Image": "https://images.indianexpress.com/2022/05/K1.jpg",
+            "category": "mix",
+            "description": "As I was waiting for someone at a Delhi hotel recently, I saw a gentleman coming my way. He sat next to me and curiously asked about the laptop I was using. “It’s a Surface Laptop Studio,” I replied. “Does Microsoft make its own laptops?” He admitted he wasn’t aware of the Surface brand but told me he liked the form factor of the Surface Laptop Studio. “It’s different from my MacBook Pro,” said the man who introduced himself as the owner of an architectural firm. This conversation just reinforced my belief that there will be takers for Microsoft’s experimental Surface Laptop Studio, which blends certain elements of the desktop Surface Studio and the premium 2-in-1 Surface Book. It’s a mobile workstation that’s also a sketching slate and a portable home theater. I have been using the Surface Laptop Studio for a few days, and I am in love with this device even though it is not perfect. Here is my review."
+          },
+          {
+            "id": '37',
+            "title": "Should yoga be done on an empty stomach? Here’s what experts say ",
+            "Image": "https://st1.bollywoodlife.com/wp-content/uploads/2022/05/Deepika-76.png",
+            "category": "mix",
+            "description": "o one will understand you or your body the way you do so make sure you listen to it, protect it, do good things for it and celebrate it,” they saidYoga is now the preferred choice of many, to stay fit, active and agile. It helps in numerous ways, be it in improving flexibility, strength and posture, or reducing stress and providing a sense of calm. Nutrition and diet are an important part of fitness along with physical exercise. On that account, it becomes necessary to understand whether yoga should be done on an empty stomach or notAnshuka Parwani, a yoga expert, and Pooja Makhija, a nutritionist took to Instagram to talk about this dilemma that many face.Pooja believes that it’s important to have something before starting with your exercise in the morning. It should be quite light and not too heavy to kickstart your metabolism.While both have contrasting opinions, they strongly agreed and upheld the point that you know your body better than anyone else. Hence, it’s important to observe and understand what your body is trying to tell you and do as one may deem fit. They said, if practising yoga on an empty stomach is suiting you better, do that. On the other hand, if it feels like you need something to get a better start, follow that as a part of your routine.    "
+          },
+          {
+            "id": '61',
+            "title": "Deepika Padukone EXUDES desi vibes with a modern touch in a white ruffle saree during the closing ceremony",
+            "Image": "https://images.indianexpress.com/2022/04/GettyImages-yoga-1200.jpg",
+            "category": "footer1",
+            "description": "Gehraiyaan actress Deepika Padukone is among the few stars who are representing India at Cannes 2022. She is churning out some fabulous fashion stints leaving all her fans mesmerised. For the closing ceremony of the international film festival, the actress opted to go all desi. But not without a modern touch.After wearing some classic modern pieces, Deepika Padukone chose a Abu Jani-Sandeep Khosla creation to end her stint at Cannes 2022.The actress looked absolutely divine as walked the red carpet wearing this beautiful saree. The neckpiece made of pearls added much glam to the sweet saree.  The actress attended the press conference with other jury members Rebecca Hall and Jeff Nichols. Sticking to her roots, she greeted everyone with a namaste.Well, if this picture does not hypnotise you then we don't know what will. Subtle makeup, pearl earrings and more - the actress looked drop-dead gorgeous as ever."
+          },
+          {
+            "id": '62',
+            "title": "Amber Heard says she has received threats 'every single day' during trial: 'People want to put my baby in a microwave' ",
+            "Image": "https://images.indianexpress.com/2022/05/Facial-search-engine-featured.jpg",
+            "category": "footer2",
+            "description": "In a video from the trial shared by Law & Crime Network on their YouTube channel, Amber told the jury, I am harassed,combat scene and a trigger happen. I have a meltdown and have to deal with that, the crew I work with have to deal with that, because of the damage I walk around with every single day. I am not sitting in this courtroom snickering. I’m not sitting in this courtroom laughing, smiling, or making snide jokes. I’m not. This is horrible. A jury is scheduled to hear the closing arguments on Friday in Johnny's lawsuit against Amber. Each side will have two hours to summarise their case in a trial that has stretched on for six weeks. Johnny Depp is suing Amber for $50 million in Virginia's Fairfax County Circuit Court over a December 2018 op-ed she wrote in The Washington Post describing herself as “a public figure representing domestic abuse”. His lawyers say he was defamed by the article even though it never mentioned his name."
+          },
+          {
+            "id": '63',
+            "title": "A face search engine anyone can use is alarmingly accurate ",
+            "Image": "https://images.hindustantimes.com/img/2022/05/27/550x309/amber_heard_1652846124301_1653656835956.JPG",
+            "category": "footer3",
+            "description": "For $29.99 a month, a website called PimEyes offers a potentially dangerous superpower from the world of science fiction: the ability to search for a face, finding obscure photos that would otherwise have been as safe as the proverbial needle in the vast digital haystack, evidently the life of every party, and in the blurry background of a photo taken of someone else at a Greek airport in 2019. A journalist’s past life in a rock band was unearthed, as was another’s preferred summer camp getaway."
+          }
+    ])
+}
+
+module.exports.apiController = get;
