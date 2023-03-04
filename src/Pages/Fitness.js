8548083cee40017082c3659f3b8c1cc2ecd5f514
Sajal4 <Sajaldebnath923@gmail.com>
2023-03-04 12:05:51 +0530

    initial

--- src/Pages/Fitness.js
+++ src/Pages/Fitness.js
@@ -6,7 +6,7 @@ const Fitness = () => {
     const[data,setData]=useState([])
     useEffect(()=>{
         const url = "https://backends-blogapp.herokuapp.com/api/fit";
-        const url = "https://blog-backend-heroku.vercel.app/api/fit";
+        // const url = "https://blog-backend-heroku.vercel.app/api/fit";
         fetch(url).then(res => res.json())
             .then(res => setData(res)  )
     },[])
